b5460859c9125298a2f1c1a88cfb7b7d5467ca0c
victorop <victordopr@gmail.com>
2023-04-12 10:46:03 -0300

    funcionalidade do slide para mobile

--- js/slides.js
+++ js/slides.js
@@ -18,25 +18,39 @@ export default class Slides {
     }
 
     updateDistance(clientX) {
-        this.dist.movement = (this.dist.startX - clientX)*1.6; //calcula movimentação (clique inicial até onde moveu o mouse); movimento *1.6 para dar o slide mais rápido
-        return this.dist.finalPosition-this.dist.movement;
+        this.dist.movement = (this.dist.startX - clientX) * 1.6; //calcula movimentação (clique inicial até onde moveu o mouse); movimento *1.6 para dar o slide mais rápido
+        return this.dist.finalPosition - this.dist.movement;
     }
-
 
 
     onStart(event) {
-        event.preventDefault(); //previne aquele comportamento de aparecer a imagem quando clicamos e arrastamos
-        this.dist.startX = event.clientX //pega a posição  no eixo x quando damos primeiro clique no slide
-        this.wrapper.addEventListener('mousemove', this.onMove);
+        let movetype;
+
+        if (event.type === 'mousedown') {
+            event.preventDefault(); //previne aquele comportamento de aparecer a imagem quando clicamos e arrastamos
+            this.dist.startX = event.clientX //pega a posição  no eixo x quando damos primeiro clique no slide
+            movetype = 'mousemove';
+        } else {
+            this.dist.startX = event.changedTouches[0].clientX; //é o primeiro toque no mobile, equivale a event.clientX
+            movetype = 'touchmove';
+
+        }
+
+
+        this.wrapper.addEventListener(movetype, this.onMove);
+
     }
 
-    onEnd() {
-        this.wrapper.removeEventListener('mousemove', this.onMove);
-        this.dist.finalPosition=this.dist.movePosition;
+    onEnd(event) {
+        const movetype = (event.type==='mouseup')?'mousemove':'touchmove';
+        this.wrapper.removeEventListener(movetype, this.onMove);
+        this.dist.finalPosition = this.dist.movePosition;
+
     }
 
     onMove(event) { //ativado apenas quando fiz o mousedown primeiro
-        const finalPosition = this.updateDistance(event.clientX);
+        const pointerClick = (event.type==='mousemove')?event.clientX:event.changedTouches[0].clientX;//op. ternário p ver se a função onMove usar o parâmetro para mousemove ou touchmove
+        const finalPosition = this.updateDistance(pointerClick);
         this.moveSlide(finalPosition)
     }
 
@@ -47,7 +61,9 @@ export default class Slides {
 
     addEvent() {
         this.wrapper.addEventListener('mousedown', this.onStart); //mousedown é quando o usuário pressiona o botão do mous
-        this.wrapper.addEventListener('mouseup', this.onEnd)
+        this.wrapper.addEventListener('touchstart', this.onStart); //touch p/ dar responsividade, utilizado no mobile
+        this.wrapper.addEventListener('mouseup', this.onEnd);
+        this.wrapper.addEventListener('touchend', this.onEnd)
     }
 
     init() {
@@ -58,3 +74,5 @@ export default class Slides {
 
 
 };
+
+//nesta aula vamos fazer a funcionalidade para mobile, já que não funciona mousedown para ela
